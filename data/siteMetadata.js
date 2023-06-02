a977f88d1a975e45c76addaa44c4313ee4d72595
Sergiu Grisca <sergiu@kuama.net>
2023-06-02 08:03:18 +0200

    :lipstick: new site logo

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -2,17 +2,17 @@
 
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Sergiu Grisca WebSite',
+  title: 'Sergiu Grisca',
   author: 'Sergiu Grisca',
   headerTitle: 'Sergiu Grisca',
-  description: 'Sergiu Grisca personal website',
+  description: 'Sergiu Grisca personal website and blog',
   language: 'en-us',
   theme: 'dark',
   siteUrl: 'https://serjdev.com/',
   siteRepo: 'https://github.com/Serj10GR/website',
   siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
-  socialBanner: '/static/images/twitter-card.png',
+  socialBanner: '/static/images/logo.png',
   email: 'sirioja92@gmail.com',
   github: 'https://github.com/Serj10GR',
   twitter: 'https://twitter.com/SergiuGrisca',
