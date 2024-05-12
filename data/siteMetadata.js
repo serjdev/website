e5e567b4493affdd4a22f41ed0f3f1df6d249e87
Sergiu Grisca <sergiu@kuama.net>
2024-05-12 16:14:26 +0200

    :sparkles: vercel analytics

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -19,14 +19,6 @@ const siteMetadata = {
   facebook: 'https://www.facebook.com/sergiu.grisca.1',
   linkedin: 'https://www.linkedin.com/in/sergiu-grisca/',
   locale: 'en-US',
-  analytics: {
-    // If you want to use an analytics provider you have to add it to the
-    // content security policy in the `next.config.js` file.
-    // supports plausible, simpleAnalytics, umami or googleAnalytics
-    plausibleDataDomain: 'https://serjdev.com/',
-    simpleAnalytics: false, // true or false
-    googleAnalyticsId: 'G-EV8RZ0YJFY',
-  },
 }
 
 module.exports = siteMetadata
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,28 @@
 import '@/css/tailwind.css'
 import '@/css/prism.css'
 import 'katex/dist/katex.css'
-// import '@/css/docsearch.css' // Uncomment if using algolia docsearch
-// import '@docsearch/css' // Uncomment if using algolia docsearch
 
 import { ThemeProvider } from 'next-themes'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import { Analytics } from '@vercel/analytics/react'
+import { SpeedInsights } from '@vercel/speed-insights/react'
 
-import siteMetadata from '@/data/siteMetadata'
-import { Analytics } from 'pliny/analytics'
 import LayoutWrapper from '@/components/LayoutWrapper'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider attribute="class" defaultTheme="dark">
-      <Head>
-        <meta content="width=device-width, initial-scale=1" name="viewport" />
-      </Head>
-      <Analytics analyticsConfig={siteMetadata.analytics} />
-      <LayoutWrapper>
-        <Component {...pageProps} />
-      </LayoutWrapper>
-    </ThemeProvider>
+    <>
+      <ThemeProvider attribute="class" defaultTheme="dark">
+        <Head>
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+        </Head>
+        <LayoutWrapper>
+          <Component {...pageProps} />
+        </LayoutWrapper>
+      </ThemeProvider>
+      <Analytics />
+      <SpeedInsights />
+    </>
   )
 }
